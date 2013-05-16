f498f45a2bc0de2458ab257ba922b8c31d32d7d3
Gonçalo Bessa <i10694@epcjc.net>
2013-05-16 11:47:57 +0100

    16 de maio
    16 de maio

--- modulo17/modulo17/Form2.Designer.vb
+++ modulo17/modulo17/Form2.Designer.vb
@@ -22,16 +22,58 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.InserirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColeçãoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InserirSeloToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InserirToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(586, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'InserirToolStripMenuItem
+        '
+        Me.InserirToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ColeçãoToolStripMenuItem, Me.InserirSeloToolStripMenuItem})
+        Me.InserirToolStripMenuItem.Name = "InserirToolStripMenuItem"
+        Me.InserirToolStripMenuItem.Size = New System.Drawing.Size(51, 20)
+        Me.InserirToolStripMenuItem.Text = "Inserir"
+        '
+        'ColeçãoToolStripMenuItem
+        '
+        Me.ColeçãoToolStripMenuItem.Name = "ColeçãoToolStripMenuItem"
+        Me.ColeçãoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ColeçãoToolStripMenuItem.Text = "Coleção"
+        '
+        'InserirSeloToolStripMenuItem
+        '
+        Me.InserirSeloToolStripMenuItem.Name = "InserirSeloToolStripMenuItem"
+        Me.InserirSeloToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.InserirSeloToolStripMenuItem.Text = "Inserir Selo"
         '
         'Form2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(828, 321)
+        Me.ClientSize = New System.Drawing.Size(586, 384)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form2"
         Me.Text = "Form2"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents InserirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ColeçãoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InserirSeloToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
 End Class
